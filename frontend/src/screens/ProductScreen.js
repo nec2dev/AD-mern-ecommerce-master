20b5c95d115e1514bcd5523965276cabebc4ae2f
nec2dev <nec2multivackergithub@outlook.com>
2021-03-06 10:11:34 -0300

    make our product screen

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Product from '../components/Product';
+import data from '../data';
+
+export default function ProductScreen() {
+    return (
+      <div>
+          Product screen
+      </div>
+    )
+}
